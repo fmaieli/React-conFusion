2506dd6e395da26e45f0e0ee949aef1559863b58
Franco Roberto Maieli <fmaieli@baufest.com>
2020-03-08 15:15:47 -0300

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,12 +4,13 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 function RenderDish({dish}) {
     return (
         <Card>
-            <CardImg width="100%" object src={dish.image} alt={dish.name} />
+            <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
